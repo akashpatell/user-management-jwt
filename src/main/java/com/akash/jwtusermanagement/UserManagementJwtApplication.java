de48c341cf594342978de3af164fb34fa813bc05
Akash Patel <ap3699591@gmail.com>
2020-07-28 12:36:08 +0530

    Initial commit

---
+++ src/main/java/com/akash/jwtusermanagement/UserManagementJwtApplication.java
@@ -0,0 +1,13 @@
+package com.akash.jwtusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/jwtusermanagement/UserManagementJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.jwtusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
